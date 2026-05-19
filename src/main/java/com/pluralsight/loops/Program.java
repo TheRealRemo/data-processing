1d707cd76e304a32020f933ca625356266771a58
TheRealRemo <timothyprice2001@gmail.com>
2026-05-19 10:11:55 -0400

    Initial commit

---
+++ src/main/java/com/pluralsight/loops/Program.java
@@ -0,0 +1,22 @@
+package com.pluralsight.loops;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Program {
+    public static void main(String[] args) {
+        List<Person> people = new ArrayList<>();
+
+        people.add(new Person("Liam", "Carter", 28));
+        people.add(new Person("Sophia", "Martinez", 34));
+        people.add(new Person("Noah", "Bennett", 19));
+        people.add(new Person("Emma", "Collins", 42));
+        people.add(new Person("James", "Walker", 31));
+        people.add(new Person("Olivia", "Reed", 25));
+        people.add(new Person("Benjamin", "Price", 37));
+        people.add(new Person("Ava", "Mitchell", 22));
+        people.add(new Person("Elijah", "Turner", 45));
+        people.add(new Person("Mia", "Parker", 29));
+
+    }
+}
