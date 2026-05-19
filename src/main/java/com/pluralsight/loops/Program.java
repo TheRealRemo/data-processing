d31b028919f3dbb38a1516ee4e290877eaa3ecc5
TheRealRemo <timothyprice2001@gmail.com>
2026-05-19 10:21:07 -0400

    added for loop and user input to find people of matching last and firstname in person array list

--- src/main/java/com/pluralsight/loops/Program.java
+++ src/main/java/com/pluralsight/loops/Program.java
@@ -2,6 +2,7 @@ package com.pluralsight.loops;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
@@ -15,8 +16,19 @@ public class Program {
         people.add(new Person("Olivia", "Reed", 25));
         people.add(new Person("Benjamin", "Price", 37));
         people.add(new Person("Ava", "Mitchell", 22));
-        people.add(new Person("Elijah", "Turner", 45));
+        people.add(new Person("Elijah", "Carter", 45));
         people.add(new Person("Mia", "Parker", 29));
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Who would you like to search for? (Use first name, or last name.): ");
+        String nameInput = scanner.nextLine();
+        List<Person> nameMatch = new ArrayList<>();
+        for (Person person : people) {
+            if (nameInput.equalsIgnoreCase(person.getFirstName()) || nameInput.equalsIgnoreCase(person.getLastName())){
+                nameMatch.add(person);
+            }
+        }
+        for (Person match : nameMatch) {
+            System.out.println(match);
+        }
     }
 }
